9487153d4423942aed8841e3eeea325637fec3fa
Yoshiki Aoki <ringohub@gmail.com>
2018-03-16 14:24:25 +0900

    :sparkle: Add codes

---
+++ src/git-origin-parser.js
@@ -0,0 +1,50 @@
+// @see https://git-scm.com/book/en/v2/Git-on-the-Server-The-Protocols
+module.exports = gitOrigin => {
+  if (/file:/.test(gitOrigin)) {
+    const parsedFile = gitOrigin.match(new RegExp('^file://(.+).git'));
+    return {
+      type: 'file',
+      path: parsedFile[1]
+    };
+  }
+
+  if (/ssh:/.test(gitOrigin)) {
+    const parsedSsh = gitOrigin.match(new RegExp('^ssh://(.+)@(.+?)/(.+).git'));
+    const origin = {
+      type: 'ssh',
+      user: parsedSsh[1],
+      domain: parsedSsh[2],
+      path: parsedSsh[3]
+    };
+    const paths = parsedSsh[3].split('/');
+    if (paths.length === 2) {
+      origin.org = paths[0];
+      origin.repo = paths[1];
+    }
+    return origin;
+  }
+
+  if (/git:/.test(gitOrigin)) {
+    const parsedGit = gitOrigin.match(new RegExp('^git://(.+?)/(.+).git'));
+    return {
+      type: 'git',
+      domain: parsedGit[1],
+      path: parsedGit[2]
+    };
+  }
+
+  if (/http(s)?:/.test(gitOrigin)) {
+    const parsedHttp = gitOrigin.match(new RegExp('^(http(?:s)?)://(.+?)/(.+).git'));
+    const originHttp = {
+      type: parsedHttp[1],
+      domain: parsedHttp[2],
+      path: parsedHttp[3]
+    };
+    const pathsHttp = parsedHttp[3].split('/');
+    if (pathsHttp.length === 2) {
+      originHttp.org = pathsHttp[0];
+      originHttp.repo = pathsHttp[1];
+    }
+    return originHttp;
+  }
+};
---
+++ test/git-origin-parser.test.js
@@ -0,0 +1,75 @@
+import test from 'ava';
+import extract from '../src/git-origin-parser';
+
+const file = 'file:///foo/bar/project.git';
+test(`Extract Local protocol like a "${file}"`, t => {
+  const origin = extract(file);
+  const expected = {
+    type: 'file',
+    path: '/foo/bar/project'
+  };
+  t.deepEqual(origin, expected);
+});
+
+const ssh = 'ssh://git@github.com/foo/bar/project.git';
+test(`Extract SSH protocol like a "${ssh}"`, t => {
+  const origin = extract(ssh);
+  const expected = {
+    type: 'ssh',
+    user: 'git',
+    domain: 'github.com',
+    path: 'foo/bar/project'
+  };
+  t.deepEqual(origin, expected);
+});
+
+const ssh2 = 'ssh://git@github.com/foo/project.git';
+test(`Extract SSH protocol including org and repo like a "${ssh2}"`, t => {
+  const origin = extract(ssh2);
+  const expected = {
+    type: 'ssh',
+    user: 'git',
+    domain: 'github.com',
+    path: 'foo/project',
+    org: 'foo',
+    repo: 'project'
+  };
+  t.deepEqual(origin, expected);
+});
+
+const git = 'git://example.com/example/repo.git';
+test(`Extract Git protocol like a "${git}"`, t => {
+  const origin = extract(git);
+  const expected = {
+    type: 'git',
+    domain: 'example.com',
+    path: 'example/repo'
+  };
+  t.deepEqual(origin, expected);
+});
+
+const https = 'https://github.com/ringohub/foo.git';
+test(`Extract HTTPS protocol including org and repo like a "${https}"`, t => {
+  const origin = extract(https);
+  const expected = {
+    type: 'https',
+    domain: 'github.com',
+    path: 'ringohub/foo',
+    org: 'ringohub',
+    repo: 'foo'
+  };
+  t.deepEqual(origin, expected);
+});
+
+const http = 'http://github.com/ringohub/foo.git';
+test(`Extract HTTP protocol including org and repo like a "${http}"`, t => {
+  const origin = extract(http);
+  const expected = {
+    type: 'http',
+    domain: 'github.com',
+    path: 'ringohub/foo',
+    org: 'ringohub',
+    repo: 'foo'
+  };
+  t.deepEqual(origin, expected);
+});
